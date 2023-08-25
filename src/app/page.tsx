dc15edaa6ca613d9fabda385e16e354eace7073e
Ermesson <ermessonlimadossantos@hotmail.com>
2023-08-25 18:29:50 -0300

    feat: create initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,66 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div
+        style={{
+          display: "flex", 
+          width: "100%",  
+          top: "0",
+          left: "0",
+          position: "absolute",
+          bottom: "0",
+          alignItems: "center", 
+        }}
+      >
+        <div
+          style={{
+            width: "100%", 
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center", 
+            padding: "1rem",
+          }}
+        >
+          <span
+            style={{
+              fontSize: "2rem",
+              marginBottom: "1rem", 
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Descubra o <strong>Caminho</strong>
+            <br />
+            <strong>Passo a Passo</strong> para se
+            <br />
+            Transformar no <strong>Dev Fullstack</strong>
+            <br />
+            que <strong>Todas as Empresas Desejam Contratar!</strong>
+          </span>
+          <button
+            style={{
+              backgroundColor: "#7FB902",
+              minWidth: "30%",
+              height: "50px",
+              borderRadius: "10px",
+              marginTop: "2rem",
+              color: "#fff",
+              fontSize: "1rem",
+              fontWeight: "bold",
+              padding: "0 1rem",
+            }}
+          >
+            Adquira seu guia para o Sucesso!
+          </button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div
+          style={{
+            width: "50%",
+          }}
+        ></div>
       </div>
     </main>
-  )
+  );
 }
